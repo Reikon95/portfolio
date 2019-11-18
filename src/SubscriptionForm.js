21350429123c0db7426e82c5013958136ace7197
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-18 11:40:02 +0000

    fixed form

--- src/SubscriptionForm.js
+++ src/SubscriptionForm.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import './SubscriptionFormStyle.css';
 
 function SubscriptionForm() {
     let [message, setMessage] = useState('');
     function handleSubmission() {
         setMessage('Thanks for submitting your details!')
-    }
+    }        
+        return <div className="container">
+        <div className="item"><input type="text" placeholder="Enter your name"/></div>
+        <div className="item"><input type="text" placeholder="Enter your email"/></div>
+        <div className="item"><input type="text" placeholder="Enter your organisation"/></div>
+        <div className="item"><button onClick={handleSubmission}>Submit</button><p>{message}</p></div>
+        {/* <div className="item">{message}</div> */}
+        </div>
 
-return <div className="submit"><input type="text" placeholder="Enter your name here"/>
-        <input type="text" placeholder="Enter your email here"/>
-        <input type="text" placeholder="Enter your organisation (if applicable) here"/>
-        <button onClick={handleSubmission}>Submit</button>{message}</div>
 
 }
 
