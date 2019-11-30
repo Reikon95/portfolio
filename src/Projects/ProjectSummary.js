f7e320b5a8ff13ca843d84ec955752cb68b70f88
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-30 11:09:06 +0000

    added component

---
+++ src/Projects/ProjectSummary.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+function ProjectSummary(props) {
+    return (
+        <>
+        <h1>{props.title}</h1>
+        <h3>Functionality</h3>
+        <p>{props.functionality}</p>
+        <h3>What I used for this project</h3>
+        <ul>
+            <li>{props.tech1}</li>
+            <li>{props.tech2}</li>
+            <li>{props.tech3}</li>
+        </ul>
+        <h3>How to view this project</h3>
+        <p>The project is deployed <a href={props.url}>here</a></p>
+        </>
+    )
+    }
+    export default ProjectSummary
--- src/Projects/WeatherChecker.js
+++ src/Projects/WeatherChecker.js
@@ -1,11 +1,9 @@
+import React, { Component } from 'react';
+import ProjectSummary from 'ProjectSummary.js';
 export default WeatherChecker() {
     return (
         <>
-        <h1>Weather Checker</h1>
-        <h3>Functionality</h3>
-        <p>An application taking advantage of the OpenWeather API. Enter the location you wish to get the weather for (or the nearest 
-            town), and you recieve a weather report.</p>
-        <h3>What I sused</h3>
+        <ProjectSummary title="Weather Checker"/>
         </>
     )
 }
